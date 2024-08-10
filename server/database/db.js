ecc6f71c956218aacacfdf1ba05db1689a428ba1
Shubham Das <113762822+ShubhamDas8981@users.noreply.github.com>
2024-08-10 13:54:45 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const Connection = async() =>{
-    const URL = `mongodb+srv://anirband553:TvQ39DkCgdGTyrrW@cluster0.jcvas3j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+    const URL = ``;
     try{
         await mongoose.connect(URL)
         console.log("DB Connected")
